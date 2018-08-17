39c04c1eaa4cefe9cf49bd27cf8d128b6269441d
Tamara <tjarataibo@gmail.com>
2018-08-17 11:39:23 -0300

    Input para búsqueda

--- src/models/app.js
+++ src/models/app.js
@@ -75,7 +75,7 @@ var markers = [];
 searchBox.addListener('places_changed', function() {
   var places = searchBox.getPlaces();
 
-  if (places.length == 0) {
+  if (places.length === 0) {
     return;
   }
 
